040ed1842affcbe4eabf48b40270d83fb2f56bc8
breenobare <breenobare2@gmail.com>
2023-07-16 20:47:00 +0300

    Create ShoppingCart.cpp

---
+++ Shopping_Cart/ShoppingCart.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+class Shopping
+{
+private:
+    int pcode;
+    float price;
+    float dis;
+    string pname;
+public:
+    void menue();
+    void administrator();
+    void buyer();
+    void add();
+    void edit();
+    void list();
+    void reciept();
+
+};
+
+void Shopping :: menue()
+{
+    int choice;
+    string email;
+    sring password;
+
+    cout<<"\t\t\t\t___________________\n";
+    cout<<"\t\t\t\t                   \n";
+    cout<<"\t\t\t\t                   \n";
+    cout<<"\t\t\t\t  Shopping Cart    \n";
+
+}
