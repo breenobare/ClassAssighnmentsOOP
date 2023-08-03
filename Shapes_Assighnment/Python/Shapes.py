d80aa05230a2d69e4bc6077409dee8f7d5606cab
breenobare <breenobare2@gmail.com>
2023-08-03 23:07:35 +0300

    Used local variables

--- Shapes_Assighnment/Python/Shapes.py
+++ Shapes_Assighnment/Python/Shapes.py
@@ -1,24 +1,18 @@
 class Shapes:
-    def __init__(self):
-        self.length = 0  # Initialize length to 0
-        self.width = 0  # Initialize width to 0
-        self.area = 0  # Initialize area to 0
-        self.perimeter = 0  # Initialize perimeter to 0
-
     def getDimensions(self):
-        self.length = int(input("Enter the length: "))  # Prompt user to enter length and store in instance variable
-        self.width = int(input("Enter the width: "))  # Prompt user to enter width and store in instance variable
+        length = int(input("Enter the length: "))  # Prompt user to enter length
+        width = int(input("Enter the width: "))  # Prompt user to enter width
+        self.compute(length, width)
 
-    def compute(self):
-        self.area = self.length * self.width  # Compute area by multiplying length and width
-        self.perimeter = 2 * (self.length + self.width)  # Compute perimeter using the formula
+    def compute(self, length, width):
+        area = length * width  # Compute area by multiplying length and width
+        perimeter = 2 * (length + width)  # Compute perimeter using the formula
+        self.display(area, perimeter)
 
-    def display(self):
-        print("Area:", self.area)  # Display the calculated area
-        print("Perimeter:", self.perimeter)  # Display the calculated perimeter
+    def display(self, area, perimeter):
+        print("Area:", area)  # Display the calculated area
+        print("Perimeter:", perimeter)  # Display the calculated perimeter
 
 if __name__ == "__main__":
     shapeObj = Shapes()  # Create an object of the Shapes class
-    shapeObj.getDimensions()  # Prompt the user to enter dimensions
-    shapeObj.compute()  # Compute the area and perimeter
-    shapeObj.display()  # Display the results
+    shapeObj.getDimensions()
