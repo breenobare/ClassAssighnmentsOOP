18f196e8cf24350822021cb3ae748fd10e22a352
breenobare <breenobare2@gmail.com>
2023-08-03 23:17:41 +0300

    Updated to local variable

--- Shapes_Assighnment/Java files/Main.java
+++ Shapes_Assighnment/Java files/Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Shapes shapeObj = new Shapes(); // Create an object of the Shapes class
         shapeObj.getDimensions(); // Prompt the user to enter dimensions
-        shapeObj.compute(); // Compute the area and perimeter
-        shapeObj.display(); // Display the results
     }
 }
--- Shapes_Assighnment/Java files/Shapes.java
+++ Shapes_Assighnment/Java files/Shapes.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 class Shapes {
-    private int length, width, area, perimeter;
-
     public void getDimensions() {
         Scanner scanner = new Scanner(System.in); // Create a Scanner object to read user input
         System.out.println("Enter the length: ");
@@ -10,14 +8,17 @@ class Shapes {
         System.out.println("Enter the width: ");
         width = scanner.nextInt(); // Read and store the width input by the user
         scanner.close(); // Close the Scanner object to release resources
+        compute(length, width);
     }
 
-    public void compute() {
+    public void compute(int length, int width) {
+        int area, perimeter;
         area = length * width; // Compute the area of the shape
         perimeter = 2 * (length + width); // Compute the perimeter of the shape
+        display(area, perimeter);
     }
 
-    public void display() {
+    public void display(int area, int perimeter) {
         System.out.println("Area: " + area); // Display the calculated area
         System.out.println("Perimeter: " + perimeter); // Display the calculated perimeter
     }
